f8bb046fc0381292fbb7ad2b9fbede1b9e804be2
Yasvanthh rajan <yasvanthrajanrajan63@gmail.com>
2026-04-16 18:36:02 +0530

    Initial clean commit - EKS Terraform project

---
+++ main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# ---------------- PROVIDER ----------------
+provider "aws" {
+  region = "us-east-1"
+}
+
+# ---------------- VPC ----------------
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+# ---------------- EKS ----------------
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "demo-eks"
+  cluster_version = "1.30"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 1
+      max_size     = 2
+      min_size     = 1
+
+      instance_types = ["t3.small"]
+
+      ami_type = "AL2_x86_64"
+
+      iam_role_additional_policies = {
+        AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+# ---------------- OUTPUTS ----------------
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_region" {
+  value = "us-east-1"
+}
